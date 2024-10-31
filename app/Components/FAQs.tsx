0b73a58432693aff21af6657c1fbc20d0f1feef6
Anna <annarosewain9@gmail.com>
2024-10-31 20:15:47 +0000

    Created a Features component and styled

---
+++ app/Components/FAQs.tsx
@@ -0,0 +1,5 @@
+export function FAQs (){
+    return (
+        <h1>FAQs</h1>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { NavBar } from "../app/Components/Navbar";
+import { Hero } from "../app/Components/Hero";
+import { Features } from "../app/Components/Features";
+import { FAQs } from "../app/Components/FAQs";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <h1>Hello</h1>
+      <Hero />
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+        <Features />
+      </div>
+      <FAQs />
     </div>
   );
 }
